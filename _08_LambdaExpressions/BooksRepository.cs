23e99b383cfaeada8274c4d77237055c03f41aeb
jttavares <jttavares@outlook.com>
2023-06-26 09:24:30 -0300

    Added LambdaExpressions in loco

---
+++ _08_LambdaExpressions/BooksRepository.cs
@@ -0,0 +1,22 @@
+﻿using _06_Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_LambdaExpressions
+{
+    public class BooksRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){Title = "Title 1", Price = 5, Isbn = "1234"},
+                new Book(){Title = "Title 2", Price = 7, Isbn = "4567"},
+                new Book(){Title = "Title 3", Price = 17, Isbn = "7890"},
+            };
+        }
+    }
+}
---
+++ _08_LambdaExpressions/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _06_Generics;
+using _08_LambdaExpressions;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Square(5));
+Func<int, int> square = Square;
+Console.WriteLine("square => " + square(10));
+
+const int factor = 5;
+
+Func<int, int> muliplier = n => n * factor;
+Console.WriteLine("multiplier => " + muliplier(10));
+var books = new BooksRepository().GetBooks();
+var cheapBooks = books.FindAll(book => book.Price < 10);
+foreach (var item in cheapBooks)
+{
+    Console.WriteLine(item.ToString());
+}
+
+//an anonymous method
+//no access modifier
+//no name
+//no return statement
+//why to use
+//for convenience
+static int Square(int number) => number * number;
